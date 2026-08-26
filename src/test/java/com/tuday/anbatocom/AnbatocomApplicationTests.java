7cde5e44fa73a515429a368f235ac5b8bde3bf01
HauPV <haupv@nextcore.vn>
2026-08-26 22:53:49 +0700

    first commit

---
+++ src/test/java/com/tuday/anbatocom/AnbatocomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuday.anbatocom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnbatocomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
